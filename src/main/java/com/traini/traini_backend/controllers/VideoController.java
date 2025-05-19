c87018ce6142b80dd3e504d27461ef61d2335807
Tomasxwav <tomas_everardo9@hotmail.com>
2025-05-19 16:41:33 -0600

    refactor: simplify video upload logic in FirebaseStorageService and clean up unused imports in VideoController

--- src/main/java/com/traini/traini_backend/controllers/VideoController.java
+++ src/main/java/com/traini/traini_backend/controllers/VideoController.java
@@ -7,11 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
-import com.traini.traini_backend.models.VideoModel;
-import com.traini.traini_backend.services.FirebaseStorageService;
 import com.traini.traini_backend.services.VideoServiceImpl;
-
 
 @RestController
 @RequestMapping("/videos")
--- src/main/java/com/traini/traini_backend/services/FirebaseStorageService.java
+++ src/main/java/com/traini/traini_backend/services/FirebaseStorageService.java
@@ -8,9 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.io.IOException;
-import java.util.UUID;
 
 @Service
 public class FirebaseStorageService {
@@ -22,19 +20,18 @@ public class FirebaseStorageService {
     private String bucketName;
 
     public String uploadVideo(MultipartFile file) throws IOException {
-        // Genera un nombre único para el archivo
-        String fileName = UUID.randomUUID().toString() + "_" + file.getOriginalFilename();
+        String fileName = file.getOriginalFilename();
 
-        // Configura el BlobId y BlobInfo
         BlobId blobId = BlobId.of(bucketName, fileName);
         BlobInfo blobInfo = BlobInfo.newBuilder(blobId)
                 .setContentType(file.getContentType())
                 .build();
 
-        // Sube el archivo a Firebase Storage
-        Blob blob = storage.create(blobInfo, file.getBytes());
+        Blob blob = storage.create(blobInfo, file.getBytes(), 
+            Storage.BlobTargetOption.predefinedAcl(Storage.PredefinedAcl.PUBLIC_READ));
 
-        // Retorna la URL pública del archivo (asegúrate de que los permisos del bucket lo permitan)
-        return String.format("https://storage.googleapis.com/%s/%s", bucketName, fileName);
+        return blob.getMediaLink();
+
+
     }
 }
