e1d750ef175d407b5b4ef42b6ba33f9b3f279f4e
Tomasxwav <tomas_everardo9@hotmail.com>
2025-06-19 18:04:01 -0600

    feat: enhance EmployeeServiceImpl and VideoServiceImpl to manage video interactions for employees

--- src/main/java/com/traini/traini_backend/services/VideoServiceImpl.java
+++ src/main/java/com/traini/traini_backend/services/VideoServiceImpl.java
@@ -8,7 +8,11 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.traini.traini_backend.enums.Department;
+import com.traini.traini_backend.models.EmployeeModel;
+import com.traini.traini_backend.models.EmployeeVideoInteractionModel;
 import com.traini.traini_backend.models.VideoModel;
+import com.traini.traini_backend.repository.EmployeeRepository;
+import com.traini.traini_backend.repository.EmployeeVideoInteractionRepository;
 import com.traini.traini_backend.repository.VideoRepository;
 import com.traini.traini_backend.services.interfaces.VideoService;
 
@@ -20,6 +24,13 @@ public class VideoServiceImpl implements VideoService {
 
     @Autowired
     private VideoRepository videoRepository; 
+
+    @Autowired
+    private EmployeeVideoInteractionRepository interactionRepository;
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+  
 
     @Override
     public String uploadAndSaveVideo(MultipartFile vildeo,MultipartFile thumbnail, String title, String description, Department category) throws Exception {
@@ -35,9 +46,27 @@ public class VideoServiceImpl implements VideoService {
         video.setThumbnailUrl(thumbnailUrl);
         video.setCategory(category); 
 
-        videoRepository.save(video);
+        VideoModel savedVideo = videoRepository.save(video);
+
+        assignVideoToDepartmentEmployees(savedVideo);
 
         return videoUrl;
+    }
+
+     private void assignVideoToDepartmentEmployees(VideoModel video) {
+        List<EmployeeModel> departmentEmployees = employeeRepository.findByDepartment(video.getCategory());
+        
+        departmentEmployees.forEach(employee -> {
+            EmployeeVideoInteractionModel interaction = new EmployeeVideoInteractionModel();
+            interaction.setEmployee(employee);
+            interaction.setVideoId(video.getId());
+            interaction.setPending(true);
+            interaction.setWatched(false);
+            interaction.setFavorite(false);
+            interaction.setLastInteractionDate(new Date());
+            
+            interactionRepository.save(interaction);
+        });
     }
 
     @Override
